96d0434ade2f0a98a1b0cb7d70e65980f4041fca
tanay-jaiman <tjaiman0907@gmail.com>
2023-11-30 19:35:04 -0700

    planning poker implementation

--- src/main/java/com/example/user_authentication/PlanningPoker.java
+++ src/main/java/com/example/user_authentication/PlanningPoker.java
@@ -1,12 +1,30 @@
 package com.example.user_authentication;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class PlanningPoker {
 
+    ArrayList<String> taskList;
+
+    @FXML
+    private TextField addTaskField;
+    @FXML
+    private TextField estimationInput;
+    @FXML
+    public Label pokerLabel;
+    @FXML
+    public Label estimationLabel;
+
+    @FXML
+    protected void addTaskFunction() {
+        pokerLabel.setText("Task - " + addTaskField.getText() + " has been added.");
+        estimationLabel.setText("The effort estimation for the task is - " + estimationInput.getText() + ".");
+    }
 }
---
+++ src/main/java/com/example/user_authentication/Task.java
@@ -0,0 +1,5 @@
+package com.example.user_authentication;
+
+public class Task {
+
+}
